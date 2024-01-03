794f7a8953c66a0f7efaef4dec15431520191699
LionShareZoki <pujic.zoran@gmail.com>
2024-01-03 20:48:02 +0100

    add group channel model

---
+++ Chat.Data/Models/GroupChannel.cs
@@ -0,0 +1,8 @@
+namespace Chat.Data.Models;
+
+public class GroupChannel
+{
+    public int ChannelId { get; set; }
+    public string ChannelName { get; set; }
+    public int UserId { get; set; }
+}
