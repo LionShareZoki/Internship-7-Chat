4c13b0bac4e8c628112306a5f418e3e7f2f1d3a6
LionShareZoki <pujic.zoran@gmail.com>
2024-01-03 20:55:14 +0100

    create group channel repository

---
+++ Chat.Domain/Repositories/GroupChannelRepository.cs
@@ -0,0 +1,9 @@
+using Chat.Data.Context;
+using Chat.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chat.Domain.Repositories
+{
+    public class GroupChannelRepository : IGroupChannelRepository
+    {
