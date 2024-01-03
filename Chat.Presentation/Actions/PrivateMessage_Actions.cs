cd3c3d470d4c77d568ecafd31d78518d7ea77c52
LionShareZoki <pujic.zoran@gmail.com>
2024-01-03 21:03:37 +0100

    create send message method

--- Chat.Presentation/Actions/PrivateMessage_Actions.cs
+++ Chat.Presentation/Actions/PrivateMessage_Actions.cs
@@ -16,3 +16,34 @@ namespace Chat.Presentation.Actions
             _contextFactory = contextFactory;
             _privateMessageRepository = new PrivateMessageRepository(contextFactory());
         }
+
+
+       public void SendMessage()
+        {
+            var users = _privateMessageRepository.GetAllUsers();
+            Console.WriteLine("Choose a recipient:");
+
+            for (int i = 0; i < users.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {users[i].Email}");
+            }
+
+            Console.Write("Enter your choice: ");
+            if (int.TryParse(Console.ReadLine(), out int choice) && choice > 0 && choice <= users.Count)
+            {
+                int recipientId = users[choice - 1].UserId;
+
+                Console.WriteLine("Enter your message:");
+                string message = Console.ReadLine();
+
+                int senderId = AuthAction.GetCurrentUserId() ?? 0;
+
+                _privateMessageRepository.SendPrivateMessage(senderId, recipientId, message);
+
+                ShowPrivateChatScreen(senderId, recipientId);
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Please try again.");
+            }
+        }
