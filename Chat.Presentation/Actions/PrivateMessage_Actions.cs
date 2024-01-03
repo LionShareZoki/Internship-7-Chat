930a691d1705ef312e4a0c53a0e525727310f636
LionShareZoki <pujic.zoran@gmail.com>
2024-01-03 21:02:56 +0100

    create private message action

---
+++ Chat.Presentation/Actions/PrivateMessage_Actions.cs
@@ -0,0 +1,10 @@
+using Chat.Domain.Repositories;
+using Chat.Data.Context;
+using System;
+using System.Linq;
+using Chat.Presentation.Interfaces;
+
+namespace Chat.Presentation.Actions
+{
+    public class PrivateMessage_Actions : IPrivateMessageActions
+    {
