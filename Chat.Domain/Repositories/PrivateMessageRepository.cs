0fa65c9a9e10199cbba4b15c3f51476806b9b7b3
LionShareZoki <pujic.zoran@gmail.com>
2024-01-03 20:57:41 +0100

    create private message repository

---
+++ Chat.Domain/Repositories/PrivateMessageRepository.cs
@@ -0,0 +1,10 @@
+using Chat.Data.Context;
+using Chat.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace Chat.Domain.Repositories
+{
+    public class PrivateMessageRepository : IPrivateMessageRepository
+    {
