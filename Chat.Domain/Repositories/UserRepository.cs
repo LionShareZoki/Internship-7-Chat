4292d091f381c1a3210fa530b3a8c4b90ae23a01
LionShareZoki <pujic.zoran@gmail.com>
2024-01-03 20:54:16 +0100

    add method to check if email is valid

--- Chat.Domain/Repositories/UserRepository.cs
+++ Chat.Domain/Repositories/UserRepository.cs
@@ -93,3 +93,9 @@ namespace Chat.Domain.Repositories
             return Password.Verify(password, user.Password);
         }
 
+        public bool IsValidEmail(string email)
+        {
+            return Regex.IsMatch(email, "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$");
+        }
+        
+        public void UpdateUserPassword(int userId, string newPassword)
