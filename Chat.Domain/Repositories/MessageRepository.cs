41cc588a994a84b9356956177c892ea205b06b3e
LionShareZoki <pujic.zoran@gmail.com>
2024-01-03 20:57:25 +0100

    create add message method

--- Chat.Domain/Repositories/MessageRepository.cs
+++ Chat.Domain/Repositories/MessageRepository.cs
@@ -13,3 +13,36 @@ public class MessageRepository : IMessageRepository
     }
 
 
+    
+    public void AddMessage(Message message)
+    {
+        try
+        {
+            using var context = _contextFactory();
+            if (message == null)
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+
+            if (message.SentAt.Kind != DateTimeKind.Utc)
+            {
+                message.SentAt = message.SentAt.ToUniversalTime();
+            }
+
+            context.Messages.Add(message);
+            context.SaveChanges();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+            if (ex.InnerException != null)
+            {
+                Console.WriteLine("Inner exception: " + ex.InnerException.Message);
+            }
+            throw;
+        }
+    }
+
+
+
+}
