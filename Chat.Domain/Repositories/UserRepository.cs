ad84bc568a417ff6e756b2fd49758e49fa5faba6
LionShareZoki <pujic.zoran@gmail.com>
2024-01-03 20:51:56 +0100

    create user repository

---
+++ Chat.Domain/Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;  // Add this for using List
+using System.Text.RegularExpressions;
+using Chat.Data.Context;
+using Chat.Data.Models;
+using Chat.Domain.Cryptography;
+
+namespace Chat.Domain.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+
+    }
+}
