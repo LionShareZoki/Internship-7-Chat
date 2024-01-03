368f0bd6832821fb3d658407c665161e078b33ae
LionShareZoki <pujic.zoran@gmail.com>
2024-01-03 21:05:52 +0100

    create show user management menu method

--- Chat.Presentation/Actions/UserManagement_Actions.cs
+++ Chat.Presentation/Actions/UserManagement_Actions.cs
@@ -16,3 +16,38 @@ namespace Chat.Presentation.Actions
             _contextFactory = contextFactory;
             _userRepository = new UserRepository(contextFactory());
         }
+
+        public void ShowUserManagementMenu()
+        {
+            var userManagementMenu = new List<string>
+            {
+                "List All Users",
+                "Delete User",
+                "Update User Email",
+                "Promote to Admin",
+                "Return to Main Menu"
+            };
+
+            while (true)
+            {
+                int choice = Program.ShowMenu("User Management Menu:", userManagementMenu);
+
+                switch (choice)
+                {
+                    case 1:
+                        ListAllUsers();
+                        break;
+                    case 2:
+                        DeleteUser();
+                        break;
+                    case 3:
+                        UpdateUserEmail();
+                        break;
+                    case 4:
+                        PromoteToAdmin();
+                        break;
+                    case 5:
+                        return;
+                }
+            }
+        }
